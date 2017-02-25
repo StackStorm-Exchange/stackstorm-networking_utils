4b0127dec26fb075e7c05cbb8c9a084a230b1a92
Jon Middleton <jon.middleton@pulsant.com>
2017-02-25 18:17:32 +0000

    Correct IP address in Google Maps URL

--- tests/test_action_geoip.py
+++ tests/test_action_geoip.py
@@ -138,7 +138,7 @@ class GeoIpActionTestCase(NetworkingUtilsBaseActionTestCase):
                     'Country': "UK",
                     'Lat': 1.0,
                     'Lon': 1.0,
-                    'Google Maps': 'https://maps.google.com/maps/place//maps/place/8.8.4.4/@1.0,1.0,10z'  # NOQA
+                    'Google Maps': 'https://maps.google.com/maps/place//maps/place/8.8.8.8/@1.0,1.0,10z'  # NOQA
                 },
                 "8.8.4.4": {
                     'AS Number': 12345,
