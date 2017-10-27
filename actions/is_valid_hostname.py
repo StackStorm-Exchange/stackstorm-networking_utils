11cb5dda6a9ab7a85ab01ca6fa2f3b014cbf2895
Kirill Izotov <enykeev@stackstorm.com>
2017-10-27 14:55:05 +0700

    Remove deprecated st2actions import

--- actions/is_valid_hostname.py
+++ actions/is_valid_hostname.py
@@ -15,7 +15,7 @@
 
 import re
 
-from st2actions.runners.pythonrunner import Action
+from st2common.runners.base_action import Action
 
 
 class IsValidHostnameAction(Action):
--- actions/is_valid_ip.py
+++ actions/is_valid_ip.py
@@ -15,7 +15,7 @@
 
 import ipaddress
 
-from st2actions.runners.pythonrunner import Action
+from st2common.runners.base_action import Action
 
 
 class IsValidIpAction(Action):
